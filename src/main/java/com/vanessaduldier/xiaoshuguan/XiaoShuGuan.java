630f6ec49898719c2ea9f086dae553368b528c60
8532187, Duldier <vanessaduldier@stud.uni-frankfurt.de>
2025-12-14 23:51:13 +0100

    Initial commit: Project setup

---
+++ src/main/java/com/vanessaduldier/xiaoshuguan/XiaoShuGuan.java
@@ -0,0 +1,29 @@
+package com.vanessaduldier.xiaoshuguan;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class XiaoShuGuan extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("XiaoShuGuan");
+        label.setStyle("-fx-font-weight: bold; -fx-padding: 20px;");
+
+        StackPane stackPane = new StackPane();
+        stackPane.getChildren().add(label);
+
+        Scene scene = new Scene(stackPane, 400, 300);
+
+        primaryStage.setTitle("XiaoShuGuan");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
